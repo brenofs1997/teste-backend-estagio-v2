256881f71db109ea59a8b510898104632066913f
PauloFH99 <brenofs1997@gmail.com>
2022-06-29 01:02:48 -0300

    Create Documentation swagger

--- teste-backend-operation-manager/src/main/java/com/api/testebackendoperationmanager/TesteBackendOperationManagerApplication.java
+++ teste-backend-operation-manager/src/main/java/com/api/testebackendoperationmanager/TesteBackendOperationManagerApplication.java
@@ -2,13 +2,13 @@ package com.api.testebackendoperationmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class TesteBackendOperationManagerApplication {
 
 	public static void main(String[] args) {
